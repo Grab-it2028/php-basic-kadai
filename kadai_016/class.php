af59f830c8779a85eb9fa1ff6c51aee988a2cb08
Grab-it2028 <163662685+Grab-it2028@users.noreply.github.com>
2024-03-24 13:15:59 +0900

    課題「クラスを2つ作ってそれぞれのインスタンスを出力しよう」の提出

---
+++ kadai_016/class.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php課題sort</title>
+</head>
+<body>
+  
+  <?php
+  class Food {
+    private $name;
+    private $price;
+
+    public function show_price(int $price) {
+      $this->price = $price;
+      echo $this->price;
+    }
+
+    public function __construct(string $name, int $price) {
+      $this->name = $name;
+      $this->price = $price;
+    }
+  }
+
+  class Animal {
+    private $name;
+    private $height;
+    private $weight;
+
+    public function show_height(int $height) {
+      $this->height = $height;
+      echo $this->height;
+    }
+
+    public function __construct(string $name, int $height, int $weight) {
+      $this->name = $name;
+      $this->height = $height;
+      $this->weight = $weight;
+    }
+  }
+  
+  $food = new Food('potato', 250);
+  $animal = new Animal('dog', 60, 5000);
+
+  print_r($food);
+  print('<br>');
+  print_r($animal);
+  print('<br>');
+  
+  $food->show_price(250);
+  print('<br>');
+  $animal->show_height(60);
+  
+
+  ?>
+  
+</body>
+</html>
